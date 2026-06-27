a648430afb1c1d1a3b240fe1da622ddeecb48347
yomgolden <yomadeogun@gmail.com>
2026-06-27 19:48:01 +0100

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,16 +1,14 @@
 """
-/start command
+Start Command
 """
 
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import (
-    Message,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-)
+from aiogram.types import Message
 
 from database.storage import upsert_user
+from utils.keyboards import home_keyboard
+from utils.formatting import home_text
 
 router = Router()
 
@@ -18,47 +16,13 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message):
 
-    # Save or update user
     await upsert_user(
         telegram_id=message.from_user.id,
-        username=message.from_user.username
-    )
-
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="📂 My Tournaments",
-                    callback_data="my_tournaments"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="➕ Create Tournament",
-                    callback_data="create_tournament"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="⚙️ Settings",
-                    callback_data="settings"
-                ),
-                InlineKeyboardButton(
-                    text="❓ Help",
-                    callback_data="help"
-                )
-            ]
-        ]
-    )
-
-    text = (
-        "<b>🏆 Tournament Manager</b>\n\n"
-        "Welcome to Tournament Manager.\n\n"
-        "Create and manage Battle Royale tournaments directly from Telegram.\n\n"
-        "Choose an option below."
+        username=message.from_user.username,
+        first_name=message.from_user.first_name
     )
 
     await message.answer(
-        text=text,
-        reply_markup=keyboard
+        text=home_text(),
+        reply_markup=home_keyboard()
     )
