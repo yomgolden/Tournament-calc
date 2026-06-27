75bd0acada9325e7e2c18608f6dc3f13f4c7c56d
yomgolden <yomadeogun@gmail.com>
2026-06-27 19:51:30 +0100

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,20 @@
+"""
+Logger
+"""
+
+import logging
+
+
+def setup_logger():
+
+    logging.basicConfig(
+
+        level=logging.INFO,
+
+        format="%(asctime)s | %(levelname)s | %(message)s"
+
+    )
+
+    return logging.getLogger(
+        "TournamentManager"
+    )
